9f2780d525e9a4c88489901dd5355ef138f69597
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-07-01 20:19:57 -0700

    Update subnet

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
     count = 3
     ami = "${var.ubuntu-ami}"
     instance_type = "${var.web-instance-type}"
-    subnet_id = "pass"
+    subnet_id = "${aws_subnet.public.id}"
     vpc_security_group_ids = ["pass"]
     key_name = "pass"
 
