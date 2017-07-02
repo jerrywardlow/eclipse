6a4e59fd1821232c07c1f4dcd530fd39d04afd23
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-07-01 20:34:17 -0700

    Database servers

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -15,3 +15,21 @@ resource "aws_instance" "web" {
         group = "eclipse-ec2"
     }
 }
+
+# Database servers
+
+resource "aws_instance" "db" {
+    count = 3
+    ami = "${var.ubuntu-ami}"
+    instance_type = "${var.db-instance-type}"
+    subnet_id = "${aws_subnet.public.id}"
+    vpc_security_group_ids = ["pass"]
+    key_name = "${aws_key_pair.eclipse.key_name}"
+
+    user_data = "pass"
+
+    tags = {
+        Name = "db${count.index}"
+        group = "eclipse-ec2"
+    }
+}
